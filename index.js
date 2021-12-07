c14c8aa5c93799b1e88bc82ab0c338c2ae012456
sukansh <psukansh@gmail.com>
2021-12-08 02:57:45 +0530

    third commit

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ let menu_bar=document.getElementById('menu-bar');
 let banner=document.getElementById('banner');
 let menu_toggle=document.getElementById('menu-toggle');
 let cross=document.getElementById('cross')
+let drop_box=document.getElementById('drop-boxes');
+
 function myFunction(){
 	menu_toggle.style.display='flex';
 	banner.style.display='none';
 	cross.style.display='block';
+	drop_box.style.display='block'
 }
 
 function myFunction1(){
