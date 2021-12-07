eb5690578a9eef203b5af982c1f60c5d70cb827b
sukansh <psukansh@gmail.com>
2021-12-08 01:41:40 +0530

    navbar chake

---
+++ index.js
@@ -0,0 +1,15 @@
+let menu_bar=document.getElementById('menu-bar');
+let banner=document.getElementById('banner');
+let menu_toggle=document.getElementById('menu-toggle');
+let cross=document.getElementById('cross')
+function myFunction(){
+	menu_toggle.style.display='flex';
+	banner.style.display='none';
+	cross.style.display='block';
+}
+
+function myFunction1(){
+	menu_toggle.style.display='none';
+	banner.style.display='flex';
+	cross.style.display='none'
+}
---
+++ index.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Dehofoo-homepage</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+
+</head>
+
+<body>
+	<div class="banner" id="banner">
+		<p>Make your first order and get flat 50% off</p>
+	</div>
+
+	<div class="up-scroll"><i class="fas fa-sort-up"><a href="#"></a></i></div>
+
+	<div class="menu-bar" id="menu-bar" onclick="myFunction()">
+		<i class="fas fa-bars"></i>
+	</div>
+
+	<div class="cross" id="cross" onclick="myFunction1()">
+		<i class="fas fa-times"></i>
+	</div>
+
+	<div class="menu-toggle" id="menu-toggle">
+		<ul>
+			<li><a href="">Home</a></li>
+			<li><a href="">About</a></li>
+			<li> <a href="">Contact</a> </li>
+			<li><a href="">Blogs</a></li>
+			<li><a href="">Home chefs</a></li>
+			<li><a href="">Join Us</a></li>
+		</ul>
+	</div>
+	<div class="container">
+
+		<section class="nav">
+			<div class="content">
+				<div class="menu_list">
+					<li> <a href="">Home</a> </li>
+					<li> <a href="">About</a> </li>
+					<li> <a href="">Contact</a> </li>
+					<div class="logo">
+						<img src="images/logo.png" alt="" width="120px" height="120px">
+					</div>
+					<li> <a href="">Blogs</a> </li>
+					<li> <a href="">Home chefs</a> </li>
+					<li> <a href="">Join Us</a> </li>
+				</div>
+
+				<div class="social">
+					<li> <a href=""><i class="fab fa-instagram"></i>instagram</a> </li>
+					<li> <a href=""><i class="fab fa-facebook"></i>facebook</a></li>
+					<li> <a href=""><i class="fab fa-twitter"></i>twitter</a></li>
+				</div>
+
+			</div>
+		</section>
+
+		<br />
+		<br />
+		<br />
+
+		<section class="homepage">
+			<div class="content">
+				<div class="info">
+					<h1>‘’Ghar ka khana direct Ghar se‘’</h1>
+					<h1> <span>Enjoy Hygienic</span> & Delicious Home Food</h1>
+					<div class="para">
+						<p><i class="fas fa-check-circle"></i>Authentic Home-made Food</p>
+						<p><i class="fas fa-check-circle"></i>Our Home-chefs are FSSAI Certified</p>
+						<p><i class="fas fa-check-circle"></i>Varieties of Healthy & Tasty Freshly Home-Cooked Food</p>
+					</div>
+				</div>
+				<div class="video">
+
+					<iframe width="500" height="315" src="https://www.youtube.com/embed/4LgRSebsh2c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+				</div>
+			</div>
+		</section>
+
+		<section class="three-cards">
+			<h2>FRESHLY <span>HOME-MADE</span>
+				FOOD FOR YOU</h2>
+			<div class="content">
+				<div class="box">
+
+					<img src="images/authentic.png" alt="" srcset="">
+					<h4>Authentic </h4>
+					<h6>Home made food</h6>
+					<p>Food prepared in restaurants is not as authentic as they may claim. Except for a few high-end places; most dishes taste the same. For all food lovers, this is a turn-off. With de’HoFoo, we connect all food lovers to home-chefs who serve authentic food, retaining the original taste and aroma of the dish.</p>
+				</div>
+				<div class="box">
+					<img src="images/fassai.png" alt="" srcset="">
+					<h4>FSSAI Certified</h4>
+					<h6>Home chefs</h6>
+					<p>Food prepared in restaurants is not as authentic as they may claim. Except for a few high-end places; most dishes taste the same. For all food lovers, this is a turn-off. With de’HoFoo, we connect all food lovers to home-chefs who serve authentic food, retaining the original taste and aroma of the dish.</p>
+
+				</div>
+				<div class="box">
+					<img src="images/veriety.png" alt="" srcset="">
+					<h4>Verirty </h4>
+					<h6>of freshly cooked food</h6>
+					<p>Food prepared in restaurants is not as authentic as they may claim. Except for a few high-end places; most dishes taste the same. For all food lovers, this is a turn-off. With de’HoFoo, we connect all food lovers to home-chefs who serve authentic food, retaining the original taste and aroma of the dish.</p>
+
+				</div>
+			</div>
+		</section>
+
+		<br />
+		<br />
+		<br />
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+
+	
+	
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+	
+	
+	
+	</div>
+
+	<script src="index.js"></script>
+
+</body>
+
+</html>
